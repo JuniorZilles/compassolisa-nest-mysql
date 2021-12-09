4e63c1002afda0724d779dc8533683ce5c3dcf1e
Junior Zilles <junior_zilles@hotmail.com>
2021-12-09 17:15:45 -0300

    fix: missing import rental module

--- src/api/modules/app/app.module.ts
+++ src/api/modules/app/app.module.ts
@@ -1,9 +1,10 @@
 import { CarsModule } from '@modules/cars/cars.module';
 import { PeopleModule } from '@modules/people/people.module';
+import { RentalModule } from '@modules/rental/rental.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [CarsModule, PeopleModule],
+  imports: [CarsModule, PeopleModule, RentalModule],
 
 })
 export class AppModule {}
