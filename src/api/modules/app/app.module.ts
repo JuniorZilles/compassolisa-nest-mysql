323b0ddaed0c19297af50c00018c2ea43648d249
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2022-01-05 10:28:59 -0300

    fix: to make to work tests

--- src/api/modules/app/app.module.ts
+++ src/api/modules/app/app.module.ts
@@ -2,7 +2,7 @@ import CarsModule from '@modules/cars/cars.module';
 import PeopleModule from '@modules/people/people.module';
 import RentalModule from '@modules/rental/rental.module';
 import { Module } from '@nestjs/common';
-import MysqlModule from 'src/database/mysql/mysql.module';
+import MysqlModule from '@modules/database/mysql/mysql.module';
 
 @Module({
   imports: [MysqlModule, CarsModule, PeopleModule, RentalModule]
